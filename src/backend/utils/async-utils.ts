321a3f8cf02960b95d9fe01ce4b95a8a01473dde
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2025-11-28 12:04:41 +0800

    added timeout logging

--- src/backend/utils/async-utils.ts
+++ src/backend/utils/async-utils.ts
@@ -2,6 +2,7 @@ export async function withTimeout<T>(promise: Promise<T>, ms: number, label?: st
   return new Promise<T>((resolve, reject) => {
     const timer = setTimeout(() => {
       const msg = label ? `Timeout waiting for ${label}` : "Operation timed out";
+      console.warn(`[withTimeout] ${msg} after ${ms}ms`);
       reject(new Error(msg));
     }, ms);
     promise.then(
