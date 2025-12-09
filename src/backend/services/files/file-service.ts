84b0f1f48e211fae71a729b495d234c494803642
Lewis Toh <lewistoh@ssw.com.au>
2025-12-09 17:18:16 +1100

    separate out into interfaces for testability

---
+++ src/backend/services/files/file-service.ts
@@ -0,0 +1,32 @@
+import type { MakeDirectoryOptions, Mode, PathLike } from "node:fs";
+import fs from "node:fs/promises";
+
+export interface IFileService {
+  access(path: PathLike): Promise<void>;
+  mkdir(path: PathLike, options?: Mode | MakeDirectoryOptions | undefined): Promise<void>;
+  readFile(path: PathLike): Promise<Buffer>;
+  writeFile(path: PathLike, data: Buffer | string): Promise<void>;
+  unlink(path: PathLike): Promise<void>;
+}
+
+export class FileService implements IFileService {
+  access(path: PathLike) {
+    return fs.access(path);
+  }
+
+  async mkdir(path: PathLike, options?: Mode | MakeDirectoryOptions | undefined): Promise<void> {
+    return await fs.mkdir(path, options).then(() => {});
+  }
+
+  readFile(path: PathLike): Promise<Buffer> {
+    return fs.readFile(path);
+  }
+
+  writeFile(path: PathLike, data: Buffer | string): Promise<void> {
+    return fs.writeFile(path, data);
+  }
+
+  unlink(path: PathLike): Promise<void> {
+    return fs.unlink(path);
+  }
+}
